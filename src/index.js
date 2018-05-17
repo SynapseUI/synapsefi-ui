f7988d422133e46b87173a702899d3fad9acabc8
Sean Hong <iamseanhong@gmail.com>
2018-05-17 15:22:30 -0700

    Add PortalModal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import button from './components/Button';
 import closeBtn from './components/CloseBtn';
 import modal from './components/Modal';
+import portalModal from './components/PortalModal';
 import * as svgIcons from './components/SvgIcons';
 import table from './components/Table';
 
@@ -11,5 +12,6 @@ export const colors = Colors;
 export const Button = button;
 export const CloseBtn = closeBtn;
 export const Modal = modal;
+export const PortalModal = portalModal;
 export const SvgIcons = svgIcons;
 export const Table = table;

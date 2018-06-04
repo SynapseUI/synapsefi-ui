80aafbfa21b80b18e740a7a1a121f75ad26affda
Jerry Xia <you@example.com>
2018-06-04 14:10:33 -0700

    Alert default test case chnaged to page level

--- example/src/App.js
+++ example/src/App.js
@@ -107,11 +107,11 @@ class App extends React.Component {
         </button>
         
         <ErrorMessage
-          // pageLevel
+          pageLevel
           hide={this.state.hideAlert}
           // success={true}
-          // error={true}
-          warning={true}
+          error={true}
+          // warning={true}
           message="Test alert message: the quick brown fox jumped over the lazy doggy"
           onClose={() => {this.setState({hideAlert: true})}}
           // this might be better if I can make close icon work without the 'hide' prop; it would have to be a stateful component

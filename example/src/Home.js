0cb94387e67ec4a4a25aba0a389e7c39bad2b456
Bo Sanders <bogeresanders@gmail.com>
2018-07-05 18:04:14 -0700

    fix

--- example/src/Home.js
+++ example/src/Home.js
@@ -22,9 +22,9 @@ import SubModalContainer from './components/SubModalContainer';
 import { Button } from '../../src/index';
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
